ec190cd7f0a990ea4884e106d64a8ccc764d202f
Hazem Elsayed <hazemm.els@gmail.com>
2020-08-31 22:16:02 +0300

    Added Boilerplate and finished Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar(props) {
+  return (
+    <div id="component-Navbar">
+      <a id="navbar-label" href="#">
+        Calculator
+      </a>
+    </div>
+  );
+}
