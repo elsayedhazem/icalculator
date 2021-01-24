81b6784fa2bb124bdad8467ce3465aeaa2021f49
Hazem Elsayed <hazemm.els@gmail.com>
2021-01-24 20:39:46 +0300

    Updates

---
+++ precache-manifest.3b140f213dbc61352c682769beabd90a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb648aa6d8c562b226e63f44d3b387c7",
+    "url": "/icalculator/index.html"
+  },
+  {
+    "revision": "db2e441f1d4aff8017d9",
+    "url": "/icalculator/static/css/main.d63ea573.chunk.css"
+  },
+  {
+    "revision": "9148f222415b29c615f6",
+    "url": "/icalculator/static/js/2.ad2c0cc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/icalculator/static/js/2.ad2c0cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2e441f1d4aff8017d9",
+    "url": "/icalculator/static/js/main.030b35bd.chunk.js"
+  },
+  {
+    "revision": "fc547b039b590dd20836",
+    "url": "/icalculator/static/js/runtime-main.8a957ff2.js"
+  }
+]);
